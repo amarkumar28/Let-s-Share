14f7373d26c106c685ed7358b39a0235313577b5
amarkumar28 <76904313+amarkumar28@users.noreply.github.com>
2021-11-14 14:42:48 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+	<!-- Footer -->
+	<div class="footer main-bg pt-5 mt-c-sm-3">
+		<div class="container">
+			<p class="text-center">Copyright &copy; Let's Share 2020</p>
+		</div>
+	</div>
+
+	<!-- Core Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/sweetalert.js"></script>
+	<script src="assets/js/notify.min.js"></script>
+	<script src="assets/js/cart.js"></script>
+	<script>
+		function cart(){
+			window.location = 'cart.php';
+		}
+	</script>
+</body>
+</html>
